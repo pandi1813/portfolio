35388207db4bd4ef5229022bfb0946b14a9d7656
pandi1813 <andrea.peter1813@gmail.com>
2023-04-16 15:13:31 +0100

    made nav buttons interactive using navlink components and passing the path as props

--- src/components/NavBar/Button/Button.jsx
+++ src/components/NavBar/Button/Button.jsx
@@ -1,7 +1,11 @@
+import { NavLink } from "react-router-dom"
+
 export default function NavButton(props) {
     return (
         <>
-        <button>{props.name}</button>
+            <NavLink to={props.path}>
+                <button>{props.name}</button>
+            </NavLink>
         </>
     )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,9 +4,9 @@ import NavButton from "./Button/Button"
 export default function NavBar() {
     return (
         <nav id="navbar">
-           <NavButton name="About"/>
-           <NavButton name="Projects"/>
-           <NavButton name="Contact"/>
+           <NavButton name="About" path="/about"/>
+           <NavButton name="Projects" path="/projects"/>
+           <NavButton name="Contact" path="contact" />
 
         </nav>
     )
