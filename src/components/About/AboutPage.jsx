21efc331bece3b18f474fdce482eb1d294582149
pandi1813 <andrea.peter1813@gmail.com>
2023-04-18 20:44:33 +0100

    added contact form template using tailwind-form

--- src/components/About/AboutPage.jsx
+++ src/components/About/AboutPage.jsx
@@ -1,5 +1,13 @@
 export default function AboutPage() {
     return (
-        <h1>About</h1>
+        <>
+            <div className="grid grid-cols-2 grid-rows-1 mt-14 mx-20" >
+                <div>
+                    <h1>Welcome</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur, incidunt laborum unde dolore porro corporis eius, ut deserunt, error eaque natus voluptatibus molestiae sint a ab iure ad. Tenetur, blanditiis voluptas. Atque, molestias libero eius ullam ea sed earum ad quas illum vel nobis debitis consequuntur? Itaque molestias possimus cupiditate.</p>
+                </div>
+                <img src="" alt="profile " />
+            </div>
+        </>
     )
 }
--- src/components/Contact/ContactPage.jsx
+++ src/components/Contact/ContactPage.jsx
@@ -1,5 +1,9 @@
+import Example from "./form/ContactForm";
+
 export default function ContactPage() {
     return (
-        <h1>Contact</h1>
+        <>
+        <Example/>
+        </>
     )
 }
