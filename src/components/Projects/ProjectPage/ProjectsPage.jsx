6d540d85d224222fdfb814cfb0e18cb5a7e5bdfb
pandi1813 <andrea.peter1813@gmail.com>
2023-04-25 22:34:13 +0100

    created json file to store project info and used map() to display them as project cards

--- src/components/Projects/ProjectPage/ProjectsPage.jsx
+++ src/components/Projects/ProjectPage/ProjectsPage.jsx
@@ -1,14 +1,13 @@
 import ProjectCard from "../ProjectCard/ProjectCard"
 
 export default function ProjectsPage(params) {
+    let projectDetails = require('../ProjectDescription.json')
     return (
         <>
         <h1>Projects</h1>
         <div className="flex justify-evenly mx-10">
 
-        <ProjectCard/>
-        <ProjectCard/>
-        <ProjectCard/>
+        {projectDetails.map(projectDetail => <ProjectCard {...projectDetail}/>)}
         </div>
         
         </>
