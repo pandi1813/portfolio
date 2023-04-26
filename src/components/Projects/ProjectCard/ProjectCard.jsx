c9ea7e7c003d5f590ae923491ed7cf2110bb799b
pandi1813 <andrea.peter1813@gmail.com>
2023-04-26 20:31:04 +0100

    added project images to gallery

--- src/components/Projects/ProjectCard/ProjectCard.jsx
+++ src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -1,18 +1,18 @@
-export default function ProjectCard() {
+export default function ProjectCard(props) {
     return (
         <>
             <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"/>
+                <img class="w-full" src={props.image} alt={props.projectName}/>
                     <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                        <div class="font-bold text-xl mb-2">{props.projectName}</div>
                         <p class="text-gray-700 text-base">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                           {props.descriptionShort}
                         </p>
                     </div>
                     <div class="px-6 pt-4 pb-2">
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{props.tech1}</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{props.tech2}</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{props.tech3}</span>
                     </div>
             </div>
         </>
