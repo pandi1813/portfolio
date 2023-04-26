736c97738a92a7d8b4a997d553ed2e6e855ea3c5
pandi1813 <andrea.peter1813@gmail.com>
2023-04-26 21:30:50 +0100

    bugfix: heading stiling now only targets the about page, does not interfere with rest of the pages

--- src/components/About/AboutPage.jsx
+++ src/components/About/AboutPage.jsx
@@ -3,11 +3,14 @@ import './about.css';
 export default function AboutPage() {
     return (
         <>
-            <div id="grid" className="grid grid-cols-2 grid-rows-2 mt-14 mx-20" >
+            <div id="grid" className="grid grid-cols-2  mt-14 mx-20" >
 
-                <h1 className="col-span-2">Welcome</h1>
-                <p id='main-text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur, incidunt laborum unde dolore porro corporis eius, ut deserunt, error eaque natus voluptatibus molestiae sint a ab iure ad. Tenetur, blanditiis voluptas. Atque, molestias libero eius ullam ea sed earum ad quas illum vel nobis debitis consequuntur? Itaque molestias possimus cupiditate.</p>
-                <h2>My skills</h2>
+                <h1 id='about-header' className="col-span-2">Welcome</h1>
+                <p id='main-text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur, incidunt laborum unde dolore porro corporis eius, ut deserunt, error eaque natus voluptatibus molestiae sint a ab iure ad. Tenetur, blanditiis voluptas. Atque, molestias libero eius ullam ea sed earum ad quas illum vel nobis debitis consequuntur? Itaque molestias possimus cupiditate.
+                <br />
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur, incidunt laborum unde dolore porro corporis eius, ut deserunt, error eaque natus voluptatibus molestiae sint a ab iure ad. Tenetur, blanditiis voluptas. Atque, molestias libero eius ullam ea sed earum ad quas illum vel nobis debitis consequuntur? Itaque molestias possimus cupiditate.
+                </p>
+                <h2 id='about-subheader'>My skills</h2>
             </div>
         </>
     )
