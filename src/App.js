4a3901242e4a529606764f138af563f6c02a1799
pandi1813 <andrea.peter1813@gmail.com>
2023-04-30 19:00:02 +0100

    changed 'BrowserRouter to HashRouter as an attempt to fix deployment issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/Home/HomePage';
 import ProjectsPage from './components/Projects/ProjectPage/ProjectsPage';
 import AboutPage from './components/About/AboutPage';
