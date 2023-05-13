ac0a69837e67fd1c40ad05062903dd212405eb9e
pandi1813 <andrea.peter1813@gmail.com>
2023-05-13 16:46:58 +0100

    added logo to navbar and linked it to homepage

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,9 +4,17 @@ import './navbar.css'
 export default function NavBar() {
     return (
         <nav id="navbar">
-           <NavButton name="About" path="/about"/>
-           <NavButton name="Projects" path="/projects"/>
-           <NavButton name="Contact" path="contact" />
+            <a href="/">
+                <div id="profile" className="flex gap-2">
+                    <img id='logo' src="./images/ap-logo-1.png" alt="logo" />
+                    <p>Andrea Peter | Front-end developer</p>
+                </div>
+            </a>
+            <div>
+                <NavButton name="About" path="/about" />
+                <NavButton name="Projects" path="/projects" />
+                <NavButton name="Contact" path="contact" />
+            </div>
 
         </nav>
     )
