995c040b74f720902e253582fc5fdc91bd1a30e5
pandi1813 <andrea.peter1813@gmail.com>
2023-05-13 14:11:05 +0100

    styled contact link logos

--- src/components/Contact/ContactPage.jsx
+++ src/components/Contact/ContactPage.jsx
@@ -1,13 +1,13 @@
 import ContactForm from "./form/ContactForm";
-
+import './contact-page.css'
 
 export default function ContactPage() {
     return (
         <>
             <ContactForm />
-            <div>
-                <a href="https://github.com/pandi1813" target="_blank" rel="noreferrer"><img src="./images/github.png" alt="GitHub logo" /></a>
-                <a href="https://www.linkedin.com/in/andrea-peter-b70a89102/"  target="_blank" rel="noreferrer"><img src="./images/linkedin.png" alt="LinkeIn logo" /></a>
+            <div className="flex justify-end gap-8">
+                <a href="https://github.com/pandi1813" target="_blank" rel="noreferrer"><img id="github" src="./images/github.png" alt="GitHub logo" /></a>
+                <a href="https://www.linkedin.com/in/andrea-peter-b70a89102/"  target="_blank" rel="noreferrer"><img id="linkedin" src="./images/linkedin.png" alt="LinkeIn logo" /></a>
             </div>
         </>
     )
