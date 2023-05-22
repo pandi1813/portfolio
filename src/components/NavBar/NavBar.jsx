7dd9394e75a9375679a64dec7e08e355bf7347c8
pandi1813 <andrea.peter1813@gmail.com>
2023-05-22 21:03:36 +0100

    bugfix: new attempt to fix routing error to home page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,16 @@
 import NavButton from "./Button/Button"
+import { Link } from 'react-router-dom';
 import './navbar.css'
 
 export default function NavBar() {
     return (
         <nav id="navbar">
-            <a href="/">
+            <Link href="/">
                 <div id="profile" className="flex gap-2">
                     <img id='logo' src="./images/ap-logo-1.png" alt="logo" />
                     <p>Andrea Peter | Front-end developer</p>
                 </div>
-            </a>
+            </Link>
             <div id="nav-buttons" className="grid grid-rows-1 grid-cols-3">
                 <NavButton className='nav-btn' name="About" path="/about" />
                 <NavButton className='nav-btn' name="Projects" path="/projects" />
