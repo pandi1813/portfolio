727ba321669979ded0cda903768fa45300d06414
pandi1813 <andrea.peter1813@gmail.com>
2023-05-22 20:56:55 +0100

    styled skills on about page and added margins to project cards

--- src/components/About/AboutPage.jsx
+++ src/components/About/AboutPage.jsx
@@ -14,19 +14,19 @@ export default function AboutPage() {
                     <span className='p-start'>I started coding</span>  when I decided I wanted to learn a new skill. I did a bit of research and came across freeCodeCamp and began their Responsive Web Design certificate, which introduced me to the basics of HTML and CSS. Once I completed that, I was hooked and knew I wanted to keep learning. I enrolled in the Front-End Web Development Boot Camp from Trilogy Education, where I recently graduated with a strong foundation in front-end web development.</p>
 
                 <ul className='flex flex-wrap justify-center '>
-                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">HTML</li>
-                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">CSS</li>
-                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">JavaScript</li>
-                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">ES6</li>
-                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">React</li>
-                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Node.js</li>
-                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">NPM</li>
-                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Git</li>
-                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">GitHub</li>
-                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Figma</li>
-                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Photoshop</li>
-                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">jQuery</li>
-                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">APIs</li>
+                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">HTML</li>
+                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">CSS</li>
+                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">JavaScript</li>
+                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">ES6</li>
+                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">React</li>
+                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Node.js</li>
+                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">NPM</li>
+                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Git</li>
+                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">GitHub</li>
+                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Figma</li>
+                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Photoshop</li>
+                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">jQuery</li>
+                    <li className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">APIs</li>
                 </ul>
                 <p id='p3'>
                     <span className='p-start'> When I'm not coding,</span> I love to explore my other passions. I'm always drawing and painting and trying to get better at it. It's a great way for me to unwind and let my creativity flow. (I started posting some of my art on my <a href='https://www.instagram.com/_p_andi_/'>Instagram</a> if you are interested)  I'm also a big fan of playing guitar and learning new songs - music has always been a huge part of my life. And when I'm not inside making art or music, you can find me outside exploring nature and going on hikes. I'm also super into science and love reading up on the latest discoveries and breakthroughs
--- src/components/Projects/ProjectPage/ProjectsPage.jsx
+++ src/components/Projects/ProjectPage/ProjectsPage.jsx
@@ -7,7 +7,7 @@ export default function ProjectsPage(params) {
         <>
         <h1 id="project-title">Projects</h1>
         <p id="project-subtitle">Some of my favorite projects for the Front-End Web Dev bootcamp including individual and group projects. These are still work in progress, I've got a few ideas I'm working on.</p>
-        <div id="card-container" className="flex justify-evenly mx-10">
+        <div id="card-container" className="flex justify-evenly mx-10 mb-20">
 
         {projectDetails.map(projectDetail => <ProjectCard {...projectDetail}/>)}
         </div>
